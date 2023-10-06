dde7138934d4d029158c3640e72b9ce0b893d94a
天将明 <1541145418@qq.com>
2023-10-06 21:58:01 +0800

    ”计划通！“ mission start！！！

---
+++ src/test/java/com/example/plangood/PlanGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plangood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
